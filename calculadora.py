43565aae8bfdc2ed8091faa3d709fab736d5f7ea
Chameison <chameison.acre@gmail.com>
2022-08-09 19:56:37 -0500

    #2 Adicionar as operações matemáticas básicas

--- calculadora.py
+++ calculadora.py
@@ -4,3 +4,18 @@ number_2 = int(input('Digite o segundo número: '))
 
 print('{} + {} = '.format(number_1, number_2))
 print(number_1 + number_2)
+
+#Subtração
+
+print('{} - {} = '.format(number_1, number_2))
+print(number_1 - number_2)
+
+#Multiplicação
+
+print('{} * {} = '.format(number_1, number_2))
+print(number_1 * number_2)
+
+#Divisão
+
+print('{} / {} = '.format(number_1, number_2))
+print(number_1 + number_2)
