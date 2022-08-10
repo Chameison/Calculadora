5382b13ca58453536146aad1d48b84e5e08d7d7f
Chameison <chameison.acre@gmail.com>
2022-08-10 15:57:58 -0500

    #3 usuario seleciona operação

--- calculadora.py
+++ calculadora.py
@@ -1,21 +1,38 @@
+
+operacao = input("Digite qual operação deseja realizar: \n + - Adição \n - - Subtração \n*- Multiplicação \n/ - Divisão")
+
 number_1 = int(input('Digite o primeiro número: '))
 number_2 = int(input('Digite o segundo número: '))
 
+if operacao == '+':
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1 + number_2)
+elif operacao == '-':
+    print('{} - {} = '.format(number_1, number_2))
+    print(number_1 - number_2)
+elif operacao == '*':
+    print('{} * {} = '.format(number_1, number_2))
+    print(number_1 * number_2)
+elif operacao == '/':
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1 / number_2)
+else:
+    print("Voce tem que digitar um operador valido, de acordo com o menu apresentado, tente novamente!")
 
-print('{} + {} = '.format(number_1, number_2))
-print(number_1 + number_2)
+
+
+
+
+
+
 
 #Subtração
 
-print('{} - {} = '.format(number_1, number_2))
-print(number_1 - number_2)
+
 
 #Multiplicação
 
-print('{} * {} = '.format(number_1, number_2))
-print(number_1 * number_2)
+
 
 #Divisão
 
-print('{} / {} = '.format(number_1, number_2))
-print(number_1 / number_2)
